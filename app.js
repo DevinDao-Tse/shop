bccf7b78266004edce6b9966b7b9d1e548d4e38e
Devin Dao-Tse <devindaotse@gmail.com>
2021-09-06 14:46:01 -0400

    revamped css

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const path = require('path')
 
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminRoute)
 app.use(shopRoute)
 
