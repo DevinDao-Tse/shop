9a83f0c4882b7d96282147401d96573556bd81d0
Devin Dao-Tse <devindaotse@gmail.com>
2021-09-06 19:04:32 -0400

    fixed hbs

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-const express = require('express')
-const app = express()
-const adminRoute = require('./routes/admin')
-const shopRoute = require('./routes/shops')
-const path = require('path')
+const path = require('path');
 
-//setting HTML templatte engine to pug
-//setting views for template
-app.set('view engine', 'pug')
-app.set('views', 'views')
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
+const app = express();
 
-//middlewares, routes, packages
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
-app.use('/admin', adminRoute.routes)
-app.use(shopRoute)
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//handling 404 error
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  res.status(404).render('404', { docTitle: '404' })
-})
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
-app.listen(3000, () => {
-  console.log(`Listening on PORT 3000`)
-})
+app.listen(3000);
