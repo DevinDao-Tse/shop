7c9c1407f6e37f4f511c811ec59ceeac9981a936
Devin Dao-Tse <devindaotse@gmail.com>
2021-09-07 12:31:49 -0400

    starting based model

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 const path = require('path');
-
 const express = require('express');
-const bodyParser = require('body-parser');
 const expressHbs = require('express-handlebars');
-
 const app = express();
-
+const { get404Page } = require('./controllers/error')
 // app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs' }));
 // app.set('view engine', 'hbs');
 // app.set('view engine', 'pug');
+
 app.set('view engine', 'ejs')
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminData);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(get404Page);
 
 app.listen(3000);
