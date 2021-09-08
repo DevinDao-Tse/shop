aeb4521e1b5a2a7a6ff4134ab65fa11241fd94b2
Devin Dao-Tse <devindaotse@gmail.com>
2021-09-08 16:27:57 -0400

    optimizing structure

--- models/product.js
+++ models/product.js
@@ -1,4 +1,18 @@
-const products = []
+const fs = require('fs')
+const e = require('express')
+const path = require('path')
+const p = path.join(path.dirname(require.main.filename),
+  'data',
+  'products.json'
+)
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) return cb([])
+
+    cb(JSON.parse(fileContent))
+  })
+}
+
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +20,16 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this)
+    getProductsFromFile(products => {
+      products.push(this)
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err)
+      })
+    })
   }
 
-  static fetchAll() {
-    return products
+  static fetchAll(cb) {
+    getProductsFromFile(cb)
+    // return products
   }
 }
