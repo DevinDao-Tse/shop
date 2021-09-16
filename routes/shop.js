f43af9f868e6e15ee6ab9451283474bc34eabab1
Devin Dao-Tse <devindaotse@gmail.com>
2021-09-16 17:32:08 -0400

    add order view (empty)

--- routes/shop.js
+++ routes/shop.js
@@ -3,13 +3,14 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
-const { getProducts, getIndex, getCart, getCheckout } = require('../controllers/shop')
+const { getProducts, getIndex, getCart, getCheckout, getOrders, } = require('../controllers/shop')
 
 
 router.get('/', getIndex);
 router.get('/products', getProducts)
 router.get('/cart', getCart)
 router.get('/checkout', getCheckout)
+router.get('/orders', getOrders)
 
 
 module.exports = router;
